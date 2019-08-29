e4195a49c59210e0fff89ede28c48154f85c1a3c
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-08-29 17:36:58 -0300

    Update map.r
    Melhorando layout.

--- map.r
+++ map.r
@@ -1,4 +1,4 @@
-# Carrega as extensões RGDAL, TMAP
+# carrega as extensões RGDAL, TMAP
 library(rgdal)
 library(tmap)
 
@@ -14,13 +14,15 @@ pontos <- read.csv2("~/Desktop/localidades.csv")
 coordinates(pontos)<-~Long+Lat
 
 #Plota o mapa utilizando o pacote TMAP
+titulo = "Renda Média"
+
 #Primeira layer = Mapa de SP
 tm_shape(mapa) +
   #Utiliza as cores para montar o mapa de calor baseado na coluna RENDA
-  tm_fill(col = "RENDA") +
+  tm_fill(col = "RENDA", title = titulo, palette = "BuGn", n = 3) +
   #Insere bordas para facilitar a visualização das áreas censitárias
   tm_borders() + 
   #Insere a segunda layer = Localidades (lat,lon)
   tm_shape(pontos) + 
   #Desenha os pontos
-  tm_dots()
+  tm_dots(size = 0.05)
