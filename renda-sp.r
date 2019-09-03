d353ee2073db966cfacfc5b410a860f3ebc1b65b
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-09-03 17:04:13 -0300

    Update renda-sp.r
    Adicionando pontos.

--- renda-sp.r
+++ renda-sp.r
@@ -11,17 +11,35 @@ sp <- subset(mapa, !is.na(V005))
 
 plot(sp)
 
+#Carrega CSV de pontos
+pontos <- read.csv2("~/Desktop/localizacoes.csv")
+
+#Transforma o dataframe de pontos em SpatialPointsDataFrame utilizando o pacote SP
+coordinates(pontos)<-~Long+Lat
+
 #Muda o modo de visualização de estático para interativo (o Tmap chama o Leaflet)
 tmap_mode("view")
 
 #Seta o título do gráfico
 titulo = "Renda - SP"
 
+#Seta o os valores das quebras na legenda
+breaks = c(1000, 3000, 5000, 10000, 20000, 75000) 
+
 #Primeira layer = Mapa de SP
 tm_shape(sp) +
 
   #Utiliza as cores para montar o mapa de calor baseado na coluna V005
-  tm_fill(col = "V005", title = titulo, palette = "BuGn", n=8) +
+  tm_fill(col = "V005", title = titulo, palette = "BuGn", breaks = breaks) +
   
   #Insere bordas para facilitar a visualização das áreas
-  tm_borders() + tm_style("cobalt")
+  tm_borders() +
+
+  #Adiciona o estilo cobalt
+  tm_style("cobalt") +
+
+  #Insere a segunda layer = Localidades (lat,lon)
+  tm_shape(pontos) + 
+
+  #Desenha os pontos
+  tm_dots(size = 0.05)
